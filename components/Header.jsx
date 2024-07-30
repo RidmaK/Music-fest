498c44f285965b9d82cdcd869dc4645b1a87c791
Ridma Athukorala <athukoralaridma@gmail.com>
2024-07-30 21:27:07 +0530

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+import Image from "next/image";
+
+const Header = () => (
+  <>
+    <header>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Contacts</a>
+        <a href="">Info</a>
+      </nav>
+    </header>
+
+    <div className="carousel">
+      <div className="list">
+        <div className="item">
+          <img src="image/img1.jpg" />
+          <div className="content">
+            <div className="author">LUNDEV</div>
+            <div className="title">DESIGN SLIDER</div>
+            <div className="topic">ANIMAL</div>
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+              sequi, rem magnam nesciunt minima placeat, itaque eum neque
+              officiis unde, eaque optio ratione aliquid assumenda facere ab et
+              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
+              nisi reprehenderit tempora at laborum natus unde. Ut,
+              exercitationem eum aperiam illo illum laudantium?
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              <button>SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img2.jpg" />
+          <div className="content">
+            <div className="author">LUNDEV</div>
+            <div className="title">DESIGN SLIDER</div>
+            <div className="topic">ANIMAL</div>
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+              sequi, rem magnam nesciunt minima placeat, itaque eum neque
+              officiis unde, eaque optio ratione aliquid assumenda facere ab et
+              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
+              nisi reprehenderit tempora at laborum natus unde. Ut,
+              exercitationem eum aperiam illo illum laudantium?
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              <button>SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img3.jpg" />
+          <div className="content">
+            <div className="author">LUNDEV</div>
+            <div className="title">DESIGN SLIDER</div>
+            <div className="topic">ANIMAL</div>
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+              sequi, rem magnam nesciunt minima placeat, itaque eum neque
+              officiis unde, eaque optio ratione aliquid assumenda facere ab et
+              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
+              nisi reprehenderit tempora at laborum natus unde. Ut,
+              exercitationem eum aperiam illo illum laudantium?
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              <button>SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img4.jpg" />
+          <div className="content">
+            <div className="author">LUNDEV</div>
+            <div className="title">DESIGN SLIDER</div>
+            <div className="topic">ANIMAL</div>
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+              sequi, rem magnam nesciunt minima placeat, itaque eum neque
+              officiis unde, eaque optio ratione aliquid assumenda facere ab et
+              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
+              nisi reprehenderit tempora at laborum natus unde. Ut,
+              exercitationem eum aperiam illo illum laudantium?
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              <button>SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="thumbnail">
+        <div className="item">
+          <img src="image/img1.jpg" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img2.jpg" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img3.jpg" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="image/img4.jpg" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="arrows">
+        <button id="prev">{`<`}</button>
+        <button id="next">{`>`}</button>
+      </div>
+      <div className="time"></div>
+    </div>
+  </>
+);
+
+export default Header;
---
+++ components/index.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import { TypingText, TitleText } from "./CustomTexts";
+import ExploreCard from "./ExploreCard";
+import StartSteps from "./StartSteps";
+import NewFeatures from "./NewFeatures";
+import InsightCard from "./InsightCard";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export {
+  Navbar,
+  Header,
+  TitleText,
+  TypingText,
+  StartSteps,
+  NewFeatures,
+  ExploreCard,
+  InsightCard,
+  Footer,
+};
