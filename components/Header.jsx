3d17d68d57888ccb4486f2c6448b4b6a21825a63
Ridma Athukorala <athukoralaridma@gmail.com>
2024-07-31 12:26:37 +0530

    changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,145 +1,180 @@
 "use client";
 
-import { motion } from "framer-motion";
-
-import styles from "../styles";
-import { navVariants } from "../utils/motion";
+import { useEffect } from "react";
 import Image from "next/image";
 
-const Header = () => (
-  <>
-    <header>
-      <nav>
-        <a href="">Home</a>
-        <a href="">Contacts</a>
-        <a href="">Info</a>
-      </nav>
-    </header>
+const Header = () => {
+  useEffect(() => {
+    // JavaScript code to handle the carousel functionality
+    let nextDom = document.getElementById("next");
+    let prevDom = document.getElementById("prev");
 
-    <div className="carousel">
-      <div className="list">
-        <div className="item">
-          <img src="/1.jpg" />
-          <div className="content">
-            <div className="title">MUSIC FESTIVAL</div>
-            <div className="topic">EVOLVING</div>
-            <div className="des">
-              EVOLVING redefines the music festival experience, offering a
-              unique investment opportunity. Inspired by globally renowned
-              events like Afterlife, EVOLVING curates an immersive journey that
-              transcends the ordinary. Our carefully selected lineup of
-              international and local artists, combined with cutting-edge
-              production and a breathtaking outdoor setting, creates an
-              unparalleled atmosphere for attendees.
-            </div>
-            {/* <div className="buttons">
-              <button>SEE MORE</button>
-              <button>SUBSCRIBE</button>
-            </div> */}
-          </div>
-        </div>
-        <div className="item">
-          <img src="/2.jpg" />
-          <div className="content">
-            <div className="title">MUSIC FESTIVAL</div>
-            <div className="topic">EVOLVING</div>
-            <div className="des">
-              EVOLVING redefines the music festival experience, offering a
-              unique investment opportunity. Inspired by globally renowned
-              events like Afterlife, EVOLVING curates an immersive journey that
-              transcends the ordinary. Our carefully selected lineup of
-              international and local artists, combined with cutting-edge
-              production and a breathtaking outdoor setting, creates an
-              unparalleled atmosphere for attendees. Yes! EVOLVING is more than
-              just a festival; it's a brand that fosters a loyal community and
-              generates revenue through diverse channels, including ticketing,
-              merchandise, and brand partnerships. Investing in EVOLVING means
-              tapping into a thriving market and aligning with a concept that
-              resonates with music enthusiasts worldwide.
-            </div>
-            {/* <div className="buttons">
-              <button>SEE MORE</button>
-              <button>SUBSCRIBE</button>
-            </div> */}
-          </div>
-        </div>
-        <div className="item">
-          <img src="/3.jpg" />
-          <div className="content">
-            <div className="title">MUSIC FESTIVAL</div>
-            <div className="topic">EVOLVING</div>
-            <div className="des">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-              sequi, rem magnam nesciunt minima placeat, itaque eum neque
-              officiis unde, eaque optio ratione aliquid assumenda facere ab et
-              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
-              nisi reprehenderit tempora at laborum natus unde. Ut,
-              exercitationem eum aperiam illo illum laudantium?
-            </div>
-            {/* <div className="buttons">
-              <button>SEE MORE</button>
-              <button>SUBSCRIBE</button>
-            </div> */}
-          </div>
-        </div>
-        <div className="item">
-          <img src="/4.jpg" />
-          <div className="content">
-            <div className="title">MUSIC FESTIVAL</div>
-            <div className="topic">EVOLVING</div>
-            <div className="des">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-              sequi, rem magnam nesciunt minima placeat, itaque eum neque
-              officiis unde, eaque optio ratione aliquid assumenda facere ab et
-              quasi ducimus aut doloribus non numquam. Explicabo, laboriosam
-              nisi reprehenderit tempora at laborum natus unde. Ut,
-              exercitationem eum aperiam illo illum laudantium?
-            </div>
-            {/* <div className="buttons">
-              <button>SEE MORE</button>
-              <button>SUBSCRIBE</button>
-            </div> */}
-          </div>
-        </div>
-      </div>
-      <div className="thumbnail">
-        <div className="item">
-          <img src="/1.jpg" />
-          <div className="content">
-            <div className="title">Name Slider</div>
-            <div className="description">Description</div>
-          </div>
-        </div>
-        <div className="item">
-          <img src="/2.jpg" />
-          <div className="content">
-            <div className="title">Name Slider</div>
-            <div className="description">Description</div>
-          </div>
-        </div>
-        <div className="item">
-          <img src="/3.jpg" />
-          <div className="content">
-            <div className="title">Name Slider</div>
-            <div className="description">Description</div>
-          </div>
-        </div>
-        <div className="item">
-          <img src="/4.jpg" />
-          <div className="content">
-            <div className="title">Name Slider</div>
-            <div className="description">Description</div>
-          </div>
-        </div>
-      </div>
+    let carouselDom = document.querySelector(".carousel");
+    let SliderDom = carouselDom.querySelector(".carousel .list");
+    let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+    let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+    let timeDom = document.querySelector(".carousel .time");
 
-      <div className="arrows">
-        <button id="prev">{`<`}</button>
-        <button id="next">{`>`}</button>
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    let timeRunning = 3000;
+    let timeAutoNext = 7000;
+
+    nextDom.onclick = function () {
+      showSlider("next");
+    };
+
+    prevDom.onclick = function () {
+      showSlider("prev");
+    };
+
+    let runTimeOut;
+    let runNextAuto = setTimeout(() => {
+      nextDom.click();
+    }, timeAutoNext);
+
+    function showSlider(type) {
+      let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+      let thumbnailItemsDom = document.querySelectorAll(
+        ".carousel .thumbnail .item"
+      );
+
+      if (type === "next") {
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add("next");
+      } else {
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(
+          thumbnailItemsDom[thumbnailItemsDom.length - 1]
+        );
+        carouselDom.classList.add("prev");
+      }
+      clearTimeout(runTimeOut);
+      runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove("next");
+        carouselDom.classList.remove("prev");
+      }, timeRunning);
+
+      clearTimeout(runNextAuto);
+      runNextAuto = setTimeout(() => {
+        nextDom.click();
+      }, timeAutoNext);
+    }
+  }, []);
+
+  return (
+    <>
+      <header>
+        <nav>
+          <a href="">Home</a>
+          <a href="">Contacts</a>
+          <a href="">Info</a>
+        </nav>
+      </header>
+
+      <div className="carousel">
+        <div className="list">
+          <div className="item">
+            <img src="/1.jpg" />
+            <div className="overlay"></div>
+            <div className="content">
+              <div className="title">MUSIC FESTIVAL</div>
+              <div className="topic">EVOLVING</div>
+              <div className="des">
+                EVOLVING redefines the music festival experience, offering a
+                unique investment opportunity. Inspired by globally renowned
+                events like Afterlife, EVOLVING curates an immersive journey
+                that transcends the ordinary. Our carefully selected lineup of
+                international and local artists, combined with cutting-edge
+                production and a breathtaking outdoor setting, creates an
+                unparalleled atmosphere for attendees.
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/2.jpg" />
+            <div className="overlay"></div>
+            <div className="content">
+              <div className="title">Organizing by Noiyse</div>
+              <div className="topic">EVOLVING</div>
+              <div className="des">
+                iIt’s about to be electrified by Sri Lanka's very own DJ Noiyse!
+                A master at igniting the dance floor, Noiyse has shared the
+                stage with top local and international talent. His signature
+                sound is guaranteed to keep you moving all night long at
+                EVOLVING!
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/3.jpeg" />
+            <div className="overlay"></div>
+            <div className="content">
+              <div className="title">The Target Audience</div>
+              <div className="topic">EVOLVING</div>
+              <div className="des">
+                Music enthusiasts aged 18-40, seeking immersive experiences and
+                passionate about electronic music, drawn to both international
+                and local talent. We anticipate a vibrant crowd of 3,000
+                attendees, united by a love for music and a desire to connect
+                with others through shared passions.
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/4.jpg" />
+            <div className="overlay"></div>
+            <div className="content">
+              <div className="title">Location - Ibeach</div>
+              <div className="topic">EVOLVING</div>
+              <div className="des">
+                iBeach is a place where the music meets the waves! With stunning
+                sea views, versatile event spaces, and a reputation for
+                unforgettable parties, iBeach is the perfect backdrop to elevate
+                EVOLVING into an epic experience.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail">
+          <div className="item">
+            <img src="/1.jpg" />
+            <div className="content">
+              <div className="title">Name Slider</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/2.jpg" />
+            <div className="content">
+              <div className="title">Name Slider</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/3.jpeg" />
+            <div className="content">
+              <div className="title">Name Slider</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src="/4.jpg" />
+            <div className="content">
+              <div className="title">Name Slider</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="arrows">
+          <button id="prev">{`<`}</button>
+          <button id="next">{`>`}</button>
+        </div>
+        <div className="time"></div>
       </div>
-      <div className="time"></div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Header;
